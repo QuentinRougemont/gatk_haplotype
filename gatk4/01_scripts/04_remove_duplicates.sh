d6215611326741b1b51794e91b0cc0e2a6687b08
quentin <quentinrougemont@orange.fr>
2021-01-13 10:09:10 +0100

    update rmdup

--- gatk4/01_scripts/04_remove_duplicates.sh
+++ gatk4/01_scripts/04_remove_duplicates.sh
@@ -10,8 +10,8 @@
 #SBATCH --mem=40G
 
 # Move to directory where job was submitted
-#cd $SLURM_SUBMIT_DIR
- 
+cd $SLURM_SUBMIT_DIR
+
 #########################################################
 #AUTHOR Q. Rougemont
 #DATE : June 2019
@@ -19,6 +19,9 @@
 #INPUT: bam file
 #OUTPUT: bam file with duplicated marks
 #########################################################
+
+### load module here if needed ##
+#make sur to have picard.jar locally or in your bashrc
 
 bam=$1
 if [ $# -eq 0 ]
@@ -29,7 +32,6 @@ then
 fi
 
 # Global variables
-MARKDUPS="/prg/picard-tools/1.119/MarkDuplicates.jar"
 DEDUPFOLDER="07_deduplicated"
 METRICSFOLDER="99_metrics"
 
@@ -44,22 +46,15 @@ then
     mkdir "$METRICSFOLDER"
 fi
 
-# Copy script to log folder
-#TIMESTAMP=$(date +%Y-%m-%d_%Hh%Mm%Ss)
-#SCRIPT=$0
-#NAME=$(basename $0)
-
-# Load needed modules
-module load java/jdk/1.8.0_102
-
 # Remove duplicates from bam alignments
 for file in "$bam"
 do
-    java -Xmx30g -Djava.io.tmpdir=./tmp -jar "$MARKDUPS" \
-        INPUT="$file" \
-        OUTPUT="$DEDUPFOLDER"/$(basename "$file" .trimmed.sorted.bam).dedup.bam \
-        METRICS_FILE="$METRICSFOLDER"/metrics.txt \
-        VALIDATION_STRINGENCY=SILENT \
-        REMOVE_DUPLICATES=true \
-    TMP_DIR=./tmp
+    echo "remove duplicate for file: $bam "
+    java -Djava.io.tmpdir=./tmp -jar picard.jar  MarkDuplicates  \
+        -INPUT "$file" \
+        -OUTPUT "$DEDUPFOLDER"/$(basename "$file" .trimmed.sorted.bam).dedup.bam \
+        -METRICS_FILE "$METRICSFOLDER"/metrics.txt \
+        -VALIDATION_STRINGENCY SILENT \
+        -REMOVE_DUPLICATES true  -TMP_DIR ./tmp
 done
+
