5b065918663cf15b3729777cd68a8aaba9336e80
Quentin <quentinrougemont@orange.fr>
2021-09-27 10:53:32 +0200

    fixed typo

--- gatk4/01_scripts/computecanada_jobs/filter_parallel_arg.sh
+++ gatk4/01_scripts/computecanada_jobs/filter_parallel_arg.sh
@@ -1,24 +1,20 @@
 #!/bin/bash
-#SBATCH --account=def-blouis
+#SBATCH --account=your_account
 #SBATCH --time=10:00:00
-#SBATCH --job-name=ldhat
-#SBATCH --output=genot-%J.out
-##SBATCH --array=1-33%33
+#SBATCH --job-name=job_name
+#SBATCH --output=job-%J.out
 #SBATCH --mem=125G
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=32
-##SBATCH --gres=cpu:32
-##SBATCH --mail-user=yourmail
-##PBS -l nodes=1:ppn=8
-##SBATCH --mail-type=EA 
 
 # Move to directory where job was submitted
-#cd $SLURM_SUBMIT_DIR
+cd $SLURM_SUBMIT_DIR
 
 NCPUS=32
+#make sur to have parallel loaded or in your .bashrc
 
-input=$1 #interval.list #or chromosome list
+input=$1 #either alist of vcf splitted by intervals or a list of chromosome  or a list of intervals
 
 cat $input |\
-    parallel -j $NCPUS ./14_filter_iteration.sh {} #input
+    parallel -j $NCPUS ./01_scripts/14_filter_iteration.sh {} #input
--- gatk4/01_scripts/computecanada_jobs/gatk_parallel_arg.sh
+++ gatk4/01_scripts/computecanada_jobs/gatk_parallel_arg.sh
@@ -1,24 +1,21 @@
 #!/bin/bash
 #SBATCH --account=youraccount
 #SBATCH --time=24:00:00
-#SBATCH --job-name=ldhat
-#SBATCH --output=ldhat-%J.out
-##SBATCH --array=1-33%33
+#SBATCH --job-name=job_name
+#SBATCH --output=job-%J.out
 #SBATCH --mem=125G
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=32
-##SBATCH --gres=cpu:32
-##SBATCH --mail-user=yourmail
-##PBS -l nodes=1:ppn=8
-##SBATCH --mail-type=EA 
 
 # Move to directory where job was submitted
 cd $SLURM_SUBMIT_DIR
 
 NCPUS=32
 
+#make sur to have parallel loaded or in your .bashrc
+
 listintervals=$1 #interval.list 
 file=$2          #individual bam file to work on  
 
-cat $listintervals |parallel -j $NCPUS ./07_gatk_iteration.sh {} $file 
+cat $listintervals |parallel -j $NCPUS ./01_scripts/07_gatk_iteration.sh {} $file 
