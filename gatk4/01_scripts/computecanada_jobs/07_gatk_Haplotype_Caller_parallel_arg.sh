a6f61b2f4bd8ad31d30b11aeee075c524fd26078
Quentin Rougemont <quentinrougemont@orange.fr>
2022-03-02 21:27:28 +0100

    Update 07_gatk_Haplotype_Caller_parallel_arg.sh

--- gatk4/01_scripts/computecanada_jobs/07_gatk_Haplotype_Caller_parallel_arg.sh
+++ gatk4/01_scripts/computecanada_jobs/07_gatk_Haplotype_Caller_parallel_arg.sh
@@ -3,19 +3,24 @@
 #SBATCH --time=24:00:00
 #SBATCH --job-name=job_name
 #SBATCH --output=job-%J.out
-#SBATCH --mem=125G
+#SBATCH --mem=20G
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=32
+#SBATCH --array=1-24 #set size according to number of chromosome for instance
+##SBATCH --cpus-per-task=32
 
 # Move to directory where job was submitted
 cd $SLURM_SUBMIT_DIR
 
-NCPUS=32
-
-#make sur to have parallel loaded or in your .bashrc
+#NCPUS=32
 
 listintervals=$1 #interval.list 
 file=$2          #individual bam file to work on  
+wanted=$(sed -n "${SLURM_ARRAY_TASK_ID}p" $listintervals )
+#for SGE replace "SLURM_ARRAY_TASK_ID" by "SGE_TASK_ID" and replace the header accordingly
 
-cat $listintervals |parallel -j $NCPUS ./01_scripts/07_gatk_haplotype_caller_iteration.sh {} $file 
+#launch gatk:
+./01_scripts/07_gatk_haplotype_caller_iteration.sh $wanted $file
+
+exit
+#cat $listintervals |parallel -j $NCPUS ./01_scripts/07_gatk_haplotype_caller_iteration.sh {} $file 
