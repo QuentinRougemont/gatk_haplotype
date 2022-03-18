d87ff2983c6d7095ef79bf735e75a53425856778
Quentin Rougemont <quentinrougemont@orange.fr>
2022-03-18 08:47:36 +0100

    Update 14_filter_parallel_arg.sh
    replace parallelization by array job

--- gatk4/01_scripts/computecanada_jobs/14_filter_parallel_arg.sh
+++ gatk4/01_scripts/computecanada_jobs/14_filter_parallel_arg.sh
@@ -3,20 +3,23 @@
 #SBATCH --time=10:00:00
 #SBATCH --job-name=job_name
 #SBATCH --output=job-%J.out
-#SBATCH --mem=125G
+##SBATCH --mem=125G
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=32
+##SBATCH --cpus-per-task=32
+#SBATCH --array=1-30 #set according to nraw of intervals 
 
 # Move to directory where job was submitted
 cd $SLURM_SUBMIT_DIR
 
-NCPUS=32
-#make sur to have parallel loaded or in your .bashrc
+#NCPUS=32
 
-input=$1 #either alist of vcf splitted by intervals or a list of chromosome  or a list of intervals
+listintervals=$1 #interval list (here one id per chromosome but this can be any sort of intervals matchin gatk requirements ) 
+wanted=$(sed -n "${SLURM_ARRAY_TASK_ID}p" $listintervals )
 
 echo "filter gvcf based on GATK on $input file"
 
-cat $input |\
-    parallel -j $NCPUS ./01_scripts/14_filter_iteration.sh {} #input
+./01_scripts/14_filter_iteration.sh $listintervals
+
+#cat $input |\
+#    parallel -j $NCPUS ./01_scripts/14_filter_iteration.sh {} #input
